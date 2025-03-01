cd38251e48f759494f420f309d0c5c6a61d667cd
Yago-Torres <torresgarciayago@gmail.com>
2025-03-01 17:53:32 +0100

    cicd

--- postgre_sql/populate.sql
+++ postgre_sql/populate.sql
@@ -2,22 +2,22 @@
 TRUNCATE game_invitations, in_game_players, game_lobbies, friendship_requests, friendships, users, game_profiles CASCADE;
 
 -- 1. First game_profiles because users references it
-\copy game_profiles(username, user_stats, is_in_a_game) FROM './postgre_sql/testing_csv_files/game_profiles.csv' WITH (FORMAT CSV, HEADER, DELIMITER ',');
+\copy game_profiles(username, user_stats, is_in_a_game) FROM './testing_csv_files/game_profiles.csv' WITH (FORMAT CSV, HEADER, DELIMITER ',');
 
 -- 2. Then users
-\copy users FROM './postgre_sql/testing_csv_files/users.csv' CSV HEADER;
+\copy users FROM './testing_csv_files/users.csv' CSV HEADER;
 
 -- 3. Friendships
-\copy friendships FROM './postgre_sql/testing_csv_files/friendships.csv' CSV HEADER;
+\copy friendships FROM './testing_csv_files/friendships.csv' CSV HEADER;
 
 -- 4. Friendship requests
-\copy friendship_requests FROM './postgre_sql/testing_csv_files/friendship_requests.csv' CSV HEADER;
+\copy friendship_requests FROM './testing_csv_files/friendship_requests.csv' CSV HEADER;
 
 -- 5. Game lobbies
-\copy game_lobbies FROM './postgre_sql/testing_csv_files/game_lobbies.csv' CSV HEADER;
+\copy game_lobbies FROM './testing_csv_files/game_lobbies.csv' CSV HEADER;
 
 -- 6. In game players
-\copy in_game_players FROM './postgre_sql/testing_csv_files/in_game_players.csv' CSV HEADER;
+\copy in_game_players FROM './testing_csv_files/in_game_players.csv' CSV HEADER;
 
 -- 7. Game invitations
-\copy game_invitations FROM './postgre_sql/testing_csv_files/game_invitations.csv' CSV HEADER;
+\copy game_invitations FROM './testing_csv_files/game_invitations.csv' CSV HEADER;
