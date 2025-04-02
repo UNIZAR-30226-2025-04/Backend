ef9742632e1856f113f004f78e98f36ad8f70342
Yago-Torres <torresgarciayago@gmail.com>
2025-04-02 19:35:30 +0200

    moded go models for new indexes

--- models/postgres/user.go
+++ models/postgres/user.go
@@ -8,8 +8,8 @@ import (
  * 'User' contains the blueprint definition of a User. It contains a reference to GameProfile
  */
 type User struct {
-	Email           string    `gorm:"primaryKey;size:100;not null"`
-	ProfileUsername string    `gorm:"size:50;not null;uniqueIndex"`
+  Email           string    `gorm:"primaryKey;size:100;not null;index:idx_users_email"`
+  ProfileUsername string    `gorm:"size:50;not null;uniqueIndex:idx_users_profile_username"`
 	PasswordHash    string    `gorm:"size:255;not null"`
 	MemberSince     time.Time `gorm:"default:CURRENT_TIMESTAMP"`
 
